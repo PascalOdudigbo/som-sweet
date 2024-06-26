0dd8c632bc5c0ddb5d54e980f19607c91273b2fd
Pascal Odudigbo <odudigbopascal@gmail.com>
2024-06-26 16:30:53 +0100

    creating and styling the footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import "./_footer.scss"
+import Link from 'next/link'
 
 function Footer() {
   return (
-    <div>
-      
+    <div className='footer_main_container flex_column_center'>
+      <section className='footer_sub_container flex_row_center'>
+        <section className='opening_hours_container flex_column'>
+          <h1 className='playfair_shadow_title'>OPENING HOURS</h1>
+          <span className='flex_row_center'><p className='opening_hour'><b>Mon - Fri:</b> 9am - 6pm</p></span>
+          <span className='flex_row_center'><p className='opening_hour'><b>Saturday:</b> 9am - 4pm</p></span>
+          <span className='flex_row_center'><p className='opening_hour'><b>Sunday:</b> Closed</p></span>
+        </section>
+
+        <section className='footer_links_contaner flex_column_center'>
+          <Link className='footer_link' href="/store">Store</Link>
+          <Link className='footer_link' href="/#aboutus">About Us</Link>
+          <Link className='footer_link' href="">Store Policies</Link>
+          <Link className='footer_link' href="/contact-us">Contact Us</Link>
+        </section>
+
+        <section className='footer_contact_container'>
+          <h1 className='playfair_shadow_title'>PHONE</h1>
+          <p className='footer_phone'>+44793870248</p>
+
+          <h1 className='playfair_shadow_title'>OUR LOCATION</h1>
+          <p className='footer_location'>GL4 5XL, Old Rothman Road, Glasgow</p>
+        </section>
+
+      </section>
+      <p className='footer_copyright'>Copyright © {new Date().getFullYear()} MakkTek ltd</p>
+
     </div>
   )
 }
